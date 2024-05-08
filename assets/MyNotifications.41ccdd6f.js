a059238ef03d0e635426e46e8a213876d472bf50
Oleksii Matsui <ommatsui@knu.ua>
2024-05-08 14:03:51 +0300

    5c389f3

---
+++ assets/MyNotifications.41ccdd6f.js
@@ -0,0 +1,1 @@
+import{_ as e,K as t,W as a}from"./index.2d1c1e3f.js";const r={data(){return{messages:[]}}};function s(o,c,n,_,i,p){return t(),a("div")}var d=e(r,[["render",s]]);export{d as default};
